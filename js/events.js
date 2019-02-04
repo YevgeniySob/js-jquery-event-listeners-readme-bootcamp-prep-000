196a3ea516fc512413084dd613f3733a6a2be061
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-04 06:32:58 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -24,7 +24,7 @@ function pressIt(){
 
 function submitIt(){
   $('form').on('submit', function(){
-    aleft()
+    aleft("Your form is going to be submitted now.")
   })
 }
 $(document).ready(function(){
