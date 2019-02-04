46ec994100f10a618952ace62d1267643d0c47b5
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-04 06:15:35 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,11 +6,3 @@ $(document).ready(function(){
 
 });
 
-$("form").on("submit", function() {
-  if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
-    return;
-  }
-  alert("you entered the wrong value");
-  return;
-});
